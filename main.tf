35fe198b57aa202f9160f223069e1333d48a6954
Ferronn <ferronn@ferronn.dev>
2021-12-16 21:01:23 +0000

    remove cloud build annotations from run services

--- main.tf
+++ main.tf
@@ -16,13 +16,7 @@ resource "google_cloud_run_service" "wowcig" {
         "run.googleapis.com/client-version" = "367.0.0"
         "run.googleapis.com/execution-environment" = "gen2"
       }
-      labels = {
-        "commit-sha" = "cdc61105afa6908824cb09110fbd608bf87084bc"
-        "gcb-build-id" = "988da489-7b92-4a3d-9784-6d156d5ae156"
-        "gcb-trigger-id" = "0a38aaa3-542d-46ca-826f-3c274f4b2db0"
-        "managed-by" = "gcp-cloud-build-deploy-cloud-run"
-      }
-      name = "wowcig-00014-zom"
+      name = "wowcig-00015-aaa"
     }
     spec {
       container_concurrency = 1
@@ -60,13 +54,7 @@ resource "google_cloud_run_service" "wowless" {
         "run.googleapis.com/client-version" = "367.0.0"
         "run.googleapis.com/execution-environment" = "gen2"
       }
-      labels = {
-        "commit-sha" = "cdc61105afa6908824cb09110fbd608bf87084bc"
-        "gcb-build-id" = "b8ba6b59-5537-41ea-9c8b-5651da4817a6"
-        "gcb-trigger-id" = "3ce2e9d0-87b8-4f52-8662-33db8e57c310"
-        "managed-by" = "gcp-cloud-build-deploy-cloud-run"
-      }
-      name = "wowless-00004-rod"
+      name = "wowless-00005-aaa"
     }
     spec {
       container_concurrency = 1
@@ -104,13 +92,7 @@ resource "google_cloud_run_service" "www" {
         "run.googleapis.com/client-version" = "367.0.0"
         "run.googleapis.com/execution-environment" = "gen1"
       }
-      labels = {
-        "commit-sha" = "cdc61105afa6908824cb09110fbd608bf87084bc"
-        "gcb-build-id" = "c7543308-ef44-4311-9561-c17bceb530a5"
-        "gcb-trigger-id" = "4f5f4a58-8650-47c9-ae34-8512009cef44"
-        "managed-by" = "gcp-cloud-build-deploy-cloud-run"
-      }
-      name = "www-00012-luk"
+      name = "www-00013-aaa"
     }
     spec {
       container_concurrency = 80
